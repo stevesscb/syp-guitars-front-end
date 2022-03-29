f05462fb56b4602edef695db5ac8916da38334d2
stevesscb <stevesscb@gmail.com>
2022-03-29 19:36:14 +0800

    created footer. home page styles

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function LayoutsHeader() {
   return (
     <header className="text-center m-5">
-      <h1 style={{ fontFamily: 'Gloria Hallelujah', fontSize: '4em' }}>SYP GUITARS</h1>
+      <h1 style={{ fontFamily: 'Gloria Hallelujah', fontSize: '5em' }}>SYP GUITARS</h1>
       <h4 className="fst-italic">HK's Best Pre Owned Guitar Marketplace!</h4>
     </header>
   )
